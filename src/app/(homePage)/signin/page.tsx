252843e4812e2aa54714ee89c972316c4d98ea4a
GnussonNet <admin@gnusson.net>
2023-06-09 15:51:31 +0200

    Added sign in and sign up pages

--- src/app/(homePage)/signin/page.tsx
+++ src/app/(homePage)/signin/page.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import Link from "next/link";
+import { UserAuthForm } from "./components/UserAuthForm";
 
 const page = () => {
-  return <div>Sign In</div>;
+  return (
+    <div className="container">
+      <div className="mx-auto flex h-full w-full flex-col justify-center space-y-6 sm:max-w-sm">
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Sign in to your account
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Enter your email and password below to sign in
+          </p>
+        </div>
+        <UserAuthForm />
+        <p className="px-8 text-center text-sm text-muted-foreground">
+          By clicking continue, you agree to our{" "}
+          <Link
+            href="/terms"
+            className="underline underline-offset-4 hover:text-primary"
+          >
+            Terms of Service
+          </Link>{" "}
+          and{" "}
+          <Link
+            href="/privacy"
+            className="underline underline-offset-4 hover:text-primary"
+          >
+            Privacy Policy
+          </Link>
+          .
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default page;
